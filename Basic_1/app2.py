d14b4129a2aa06ad1c4bf68684e8d6b0f7734240
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-10-20 18:18:07 +0530

    adfd

--- Basic_1/app2.py
+++ Basic_1/app2.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name :str = "rohan"
 
 @app.get('/')
 def main():
     return "Hello World"
 
+@app.get('/get/{id}')
+def get(id : int):
+    return {'id':id}
+
+@app.get('/wet')
+def wet( limit : int = 10 , publish : bool = False , sort : Optional[str] = None):
+    if publish :
+        return {"message":f" {limit} published from db "}
+    else :
+        return {"message": f" nothing publisher since its false"}
+
+@app.post('/post')
+def main(items:Item):
+    return items
