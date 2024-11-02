93ffb75998b6b5e0f14a0a28c726060ad7180b46
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-11-02 18:18:06 +0530

    first commit

--- Basic_2/app.py
+++ Basic_2/app.py
@@ -13,7 +13,17 @@ class Forum(BaseModel):
     roll :int
     gender : Optional[str] = None
 
+
+
+
 my_post = [{"name":"hari","roll":36,"id":1}]
+
+
+def helper_delete(id):
+    for i , data in enumerate(my_post):
+        if data['id'] == id:
+            return i
+
 
 @app.post('/payload' , status_code= status.HTTP_200_OK)
 def create(payload : dict = Body(...)):
@@ -40,7 +50,16 @@ def get_post(id:int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND ,
                         detail=f"No data found on {id}")
 
-@app.get('/latest')
+@app.get('/latest' , status_code= status.HTTP_206_PARTIAL_CONTENT)
 def get_latest():
     post = my_post[len(my_post)-1]
     return {"latest post" :post}
+
+@app.delete('/delete/{id}', status_code= status.HTTP_226_IM_USED)
+def delete(id :int):
+    data =  helper_delete(id)
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND ,
+                            detail= f"Not such id : {id}")
+    data = my_post.pop(data)
+    return {"deleted":data}
